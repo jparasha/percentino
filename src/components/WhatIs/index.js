fdf3e4cc0219f4ff2ac4e839957943cca3c380bc
“jparasha” <jayantparashar@outlook.com>
2022-09-07 23:02:56 +0530

    refactor : updated logic

--- src/components/WhatIs/index.js
+++ src/components/WhatIs/index.js
@@ -1,41 +1,49 @@
 import { useState } from 'react';
 
-export default function WhatIs({show}) {
-  const [initial, setInitial] = useState('');
-  const [final, setFinal] = useState('');
+export default function WhatIs({ show }) {
+  const [initial, setInitial] = useState(0);
+  const [final, setFinal] = useState(0);
+
+  const result = Number((((initial || 0) / 100) * (final || 0)).toFixed(2));
+  const total = final + result;
 
   const onInitialChange = (event) => {
     const { target } = event || {};
-    setInitial(target?.value);
+    setInitial(+target?.value);
   };
 
   const onFinalChange = (event) => {
     const { target } = event || {};
-    setFinal(target?.value);
+    setFinal(+target?.value);
   };
 
   return (
-    show && (<div className='what-is flex'>
-      <span>what is </span>
-      <input
-        placeholder=''
-        type={'number'}
-        value={initial}
-        onChange={onInitialChange}
-      />
-      <span> percent of </span>
-      <input
-        placeholder=''
-        type={'number'}
-        value={final}
-        onChange={onFinalChange}
-      />
-      <span>
-        {' '}
-        {final &&
-          initial &&
-          `${((((initial || 0) / 100) * (final || 0)) + 100).toFixed(2) || ''}`}
-      </span>
-    </div>)
+    show && (
+      <div className='what-is flex'>
+        <span>what is </span>
+        <input
+          placeholder=''
+          type={'number'}
+          value={initial || ''}
+          onChange={onInitialChange}
+        />
+        <span> percent of </span>
+        <input
+          placeholder=''
+          type={'number'}
+          value={final || ''}
+          onChange={onFinalChange}
+        />
+        {final && initial && (
+          <div>
+            <span>{result || ''} &nbsp;</span>
+            <span>(Total </span>
+            <span className={+result > 0 ? 'text-green' : 'text-red'}>
+              {`${Number(total).toFixed(2)})`}
+            </span>
+          </div>
+        )}
+      </div>
+    )
   );
 }
